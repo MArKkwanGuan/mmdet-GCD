8b95e8054b33d94c8420c2af71a5a6ba8a940330
MArKkwanGuan <markkwan@qq.com>
2025-02-11 18:52:10 +0800

    Update __init__.py

--- mmdet/models/losses/__init__.py
+++ mmdet/models/losses/__init__.py
@@ -8,7 +8,7 @@ from .gaussian_focal_loss import GaussianFocalLoss
 from .gfocal_loss import DistributionFocalLoss, QualityFocalLoss
 from .ghm_loss import GHMC, GHMR
 from .iou_loss import (BoundedIoULoss, CIoULoss, DIoULoss, GIoULoss, IoULoss,
-                       bounded_iou_loss, iou_loss, WassersteinLoss)
+                       bounded_iou_loss, iou_loss, WassersteinLosss, Gassuian_Combination_Loss)
 from .kd_loss import KnowledgeDistillationKLDivLoss
 from .mse_loss import MSELoss, mse_loss
 from .pisa_loss import carl_loss, isr_p
@@ -26,5 +26,5 @@ __all__ = [
     'GHMR', 'reduce_loss', 'weight_reduce_loss', 'weighted_loss', 'L1Loss',
     'l1_loss', 'isr_p', 'carl_loss', 'AssociativeEmbeddingLoss',
     'GaussianFocalLoss', 'QualityFocalLoss', 'DistributionFocalLoss',
-    'VarifocalLoss', 'KnowledgeDistillationKLDivLoss', 'SeesawLoss', 'WassersteinLoss'
+    'VarifocalLoss', 'KnowledgeDistillationKLDivLoss', 'SeesawLoss', 'WassersteinLoss', 'Gassuian_Combination_Loss'
 ]
