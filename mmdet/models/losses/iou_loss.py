cb63f6e3e2e8410377c982214bb364a5651e8d69
MArKkwanGuan <markkwan@qq.com>
2025-02-11 19:35:19 +0800

    Update iou_loss.py

--- mmdet/models/losses/iou_loss.py
+++ mmdet/models/losses/iou_loss.py
@@ -548,15 +548,15 @@ def gcd_loss(pred, target, eps=1e-7, mode='exp', gamma=1):
 
     whs = center1[:, :2] - center2[:, :2]
 
-    w1 = pred[:, 2] - pred[:, 0]  + eps
-    h1 = pred[:, 3] - pred[:, 1]  + eps
-    w2 = target[:, 2] - target[:, 0]  + eps
-    h2 = target[:, 3] - target[:, 1]  + eps
+    w1 = pred[:, 2] - pred[:, 0]
+    h1 = pred[:, 3] - pred[:, 1]
+    w2 = target[:, 2] - target[:, 0]
+    h2 = target[:, 3] - target[:, 1]
 
-    center_distance1 = (whs[:, 0] / (w1 + eps)) ** 2 + (whs[:, 1] / (h1 + eps)) ** 2 + eps #
+    center_distance1 = (whs[:, 0] / (w1 + eps)) ** 2 + (whs[:, 1] / (h1 + eps)) ** 2
     wh_distance1 = (((w1 - w2) / (w2 + eps)) ** 2 + ((h1 - h2) / (h2 + eps)) ** 2) / 4
 
-    center_distance2 = (whs[:, 0] / (w2 + eps)) ** 2 + (whs[:, 1] / (h2 + eps)) ** 2#
+    center_distance2 = (whs[:, 0] / (w2 + eps)) ** 2 + (whs[:, 1] / (h2 + eps)) ** 2
     wh_distance2 = (((w1 - w2) / (w1 + eps)) ** 2 + ((h1 - h2) / (h1 + eps)) ** 2) / 4
 
     gcd_2 = ( center_distance1 + wh_distance1 + center_distance2 + wh_distance2 ) / 2 
